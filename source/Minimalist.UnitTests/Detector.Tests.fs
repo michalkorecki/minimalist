32cce44442c27d4639075c8ef39051464335b419
Michał <korecki.m@gmail.com>
2017-02-28 22:28:25 +0100

    Refactor LINQPad script into module, add test.

---
+++ source/Minimalist.UnitTests/Detector.Tests.fs
@@ -0,0 +1,24 @@
+﻿module Minimalist.UnitTests.Detector.Tests
+
+open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Reflection
+open Minimalist.Detector
+
+let loadTestData () =
+    let assembly = Assembly.GetExecutingAssembly()
+    use resourceStream = assembly.GetManifestResourceStream("11b.dat")
+    use reader = new StreamReader(resourceStream)
+    reader
+        .ReadToEnd()
+        .Split([|Environment.NewLine|], StringSplitOptions.RemoveEmptyEntries)
+
+
+[<Test>]
+let ``Maxes are found for 11B quotations (algorithm basic idea demonstration)`` () =
+    let data = loadTestData ()
+    let maxes = findMaxes loadTestData
+
+    maxes |> should haveCount 12
