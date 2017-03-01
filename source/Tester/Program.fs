d08ec9021288f85caf54b9e14348942a9e0e1878
Michał <korecki.m@gmail.com>
2017-03-01 23:47:09 +0100

    Add console tester app.

---
+++ source/Tester/Program.fs
@@ -0,0 +1,23 @@
+﻿open Minimalist.Detector
+open System
+open System.IO
+open System.Reflection
+
+let loadRecent n = fun () ->
+    let assembly = Assembly.GetExecutingAssembly()
+    use resourceStream = assembly.GetManifestResourceStream("11b.dat")
+    use reader = new StreamReader(resourceStream)
+    reader
+        .ReadToEnd()
+        .Split([|Environment.NewLine|], StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.rev
+    |> Seq.take n
+    |> Seq.rev
+    |> Seq.toArray
+
+[<EntryPoint>]
+let main argv =
+    loadRecent 100
+    |> findMaxes
+    |> Seq.iter (printfn "%A")
+    0
