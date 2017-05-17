55f6a18889b905785895eac1d5a1c987f32f23da
Michał <korecki.m@gmail.com>
2017-05-17 21:27:54 +0200

    Change tester output to more human-friendly.

--- source/Tester/Program.fs
+++ source/Tester/Program.fs
@@ -3,7 +3,7 @@ open System
 open System.IO
 open System.Reflection
 
-let loadRecent n = fun () ->
+let loadYear year = fun () ->
     let assembly = Assembly.GetExecutingAssembly()
     use resourceStream = assembly.GetManifestResourceStream("11b.dat")
     use reader = new StreamReader(resourceStream)
@@ -11,13 +11,13 @@ let loadRecent n = fun () ->
         .ReadToEnd()
         .Split([|Environment.NewLine|], StringSplitOptions.RemoveEmptyEntries)
     |> Seq.rev
-    |> Seq.take n
+    |> Seq.filter (fun q -> q.StartsWith(year.ToString()))
     |> Seq.rev
     |> Seq.toArray
 
 [<EntryPoint>]
 let main argv =
-    loadRecent 100
+    loadYear 2016
     |> findMaxes
-    |> Seq.iter (printfn "%A")
+    |> Seq.iteri (fun i q -> printfn "%i %A %.2f" i q.Date q.High)
     0
