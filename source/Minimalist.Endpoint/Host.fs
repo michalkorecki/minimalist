0a57d60854c877cadc66aee4f70c4087e74b3583
Michał <korecki.m@gmail.com>
2017-06-02 21:05:27 +0200

    Add minimalist endpoint with Nancy and Topshelf.

---
+++ source/Minimalist.Endpoint/Host.fs
@@ -0,0 +1,20 @@
+﻿module Minimalist.Endpoint.Host
+
+open Nancy
+open Nancy.Hosting.Self
+open System
+
+type MinimalistHost () =
+    let mutable host = null
+    
+    member this.Start () =
+        let configuration = new HostConfiguration()
+        configuration.UrlReservations.CreateAutomatically <- true
+        host <- new NancyHost(new Uri("http://localhost:5702"), new DefaultNancyBootstrapper(), configuration)
+        host.Start()
+    
+    member this.Stop() =
+        host.Stop()
+        host.Dispose()
+        host <- null
+    
---
+++ source/Minimalist.Endpoint/Program.fs
@@ -0,0 +1,18 @@
+﻿open Minimalist.Endpoint.Host
+open Topshelf
+
+[<EntryPoint>]
+let main argv = 
+    HostFactory.Run(fun (configuration : HostConfigurators.HostConfigurator) ->
+        configuration.Service<MinimalistHost>(fun (service : ServiceConfigurators.ServiceConfigurator<MinimalistHost>) ->
+            service.ConstructUsing(fun (name : string) -> new MinimalistHost()) |> ignore
+            service.WhenStarted(fun host -> host.Start()) |> ignore
+            service.WhenStopped(fun host -> host.Stop()) |> ignore
+        ) |> ignore
+        configuration.RunAsLocalSystem() |> ignore
+        configuration.SetDescription("Exposes extrema finding algorithm through REST API") |> ignore
+        configuration.SetDisplayName("Minimalist.Endpoint") |> ignore
+        configuration.SetServiceName("Minimalist.Endpoint") |> ignore
+    ) |> ignore
+
+    0
