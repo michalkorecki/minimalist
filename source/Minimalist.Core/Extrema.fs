b5ca400af13da0440f72b84b01671211e8946a6d
Michał <korecki.m@gmail.com>
2017-06-03 20:34:41 +0200

    Extrema endpoint working POC.

---
+++ source/Minimalist.Core/Extrema.fs
@@ -0,0 +1,52 @@
+﻿module Minimalist.Core.Extrema
+
+open System
+open System.IO
+open Minimalist.Core.Data
+open Minimalist.Core.Detector
+
+type Ticker = Ticker of string
+
+//todo: bad naming, clashing with detector code; think about domain
+type ExtremumType =
+    | Minimum
+    | Maximum
+
+type Extremum = {
+    Type : ExtremumType
+    Value : double;
+    Date : DateTime;
+}
+
+type Extrema =
+    | Extrema of seq<Extremum>
+    | Error of string
+
+let extrema ticker year =
+    let quotationsStorage = @"D:\Stock\quotes\poland_d\wse stocks"
+    try
+        let quotationsFile = sprintf "%A.txt" ticker
+        let quotationsFilePath = Path.Combine(quotationsStorage, quotationsFile)
+        use file = File.OpenRead(quotationsFilePath)
+        use reader = new StreamReader(file)
+        let quotations =
+            reader
+                .ReadToEnd()
+                .Split([|Environment.NewLine|], StringSplitOptions.RemoveEmptyEntries)
+            |> Seq.rev
+            |> Seq.map (fun line -> (line, line.Split(',')))
+            |> Seq.filter (fun (line, parts) -> parts.[0].Substring(0, 4) = year.ToString())
+            |> Seq.map fst
+            |> Seq.rev
+            |> Seq.mapi parse
+            |> Seq.toArray
+        let extrema =
+            findExtrema quotations
+            |> Seq.map (fun e ->
+                match e with
+                | Min q -> { Type = Minimum; Value = q.Low; Date = q.Date }
+                | Max q -> { Type = Maximum; Value = q.High; Date = q.Date })
+        Extrema extrema
+    with 
+        | :? FileNotFoundException -> Error "File not found"
+        | _ -> Error "Failed"
--- source/Minimalist.Endpoint/Extrema.fs
+++ source/Minimalist.Endpoint/Extrema.fs
@@ -1,16 +1,30 @@
 ﻿module Minimalist.Endpoint.Extrema
 
 open Nancy
+open Minimalist.Core.Extrema
 
 type ExtremaModule() as self =
     inherit NancyModule()
     do
+        //todo: refactor, it's not the greatest
         self.Get.[@"extrema/(?i)(?<ticker>[A-Z0-9\.]{2,})/(?<year>[0-9]{4})"] <- fun parameters ->
             let ticker = (parameters :?> Nancy.DynamicDictionary).["ticker"]
             let year = (parameters :?> Nancy.DynamicDictionary).["year"]
 
-            let result = (ticker, year)
-            let response = self.Response.AsJson([result])
+            let extrema = extrema ticker year
+            let response =
+                match extrema with
+                | Extrema ex ->
+                    let x =
+                        ex 
+                        |> Seq.map (fun e -> dict [
+                            "type", e.Type.ToString();
+                            "value", e.Value.ToString();
+                            "date", e.Date.ToString()])
+                        |> Seq.toArray
+                    self.Response.AsJson(x)                        
+                | Error message ->
+                    self.Response.AsJson([message])
             
             response :> obj
 
