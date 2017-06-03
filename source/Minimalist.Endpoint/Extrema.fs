5aa1dadda6440e5c80fb9fe3b0eed947d69f9e81
Michał <korecki.m@gmail.com>
2017-06-03 19:42:04 +0200

    Add extrema endpoint.

---
+++ source/Minimalist.Endpoint/Extrema.fs
@@ -0,0 +1,16 @@
+﻿module Minimalist.Endpoint.Extrema
+
+open Nancy
+
+type ExtremaModule() as self =
+    inherit NancyModule()
+    do
+        self.Get.[@"extrema/(?i)(?<ticker>[A-Z0-9\.]{2,})/(?<year>[0-9]{4})"] <- fun parameters ->
+            let ticker = (parameters :?> Nancy.DynamicDictionary).["ticker"]
+            let year = (parameters :?> Nancy.DynamicDictionary).["year"]
+
+            let result = (ticker, year)
+            let response = self.Response.AsJson([result])
+            
+            response :> obj
+
